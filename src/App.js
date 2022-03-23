d6b54970f7ffc6e02f91cb8d974afe6ca8930d66
WalterIran <amadorwalter3418@gmail.com>
2022-03-22 21:05:58 -0600

    Search results screen ui ready

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import SamplePage from './components/Views/Sample/SamplePage';
 import Login from './components/Views/Login/Login';
 import Profile from './components/Views/Profile/ProfilePage';
+import Search from './components/Views/Search/SearchPage';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path='/' element={<SamplePage />} />
         <Route path='/login' element={<Login />} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='/search' element={<Search />} />
       </Routes>
   );
 }
