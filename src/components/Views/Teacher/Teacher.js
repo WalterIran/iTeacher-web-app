0bbdcf16adb17b1ca8176a4bf15100e243d2228c
Oscar Andrade <oandrade57@gmail.com>
2022-03-24 18:31:10 -0600

    Teacher Registration

--- src/components/Views/Teacher/Teacher.js
+++ src/components/Views/Teacher/Teacher.js
@@ -1,51 +1,186 @@
+import './Teacher.css'
 import { BaseButton, PrimaryButton, SecondaryButton } from '../../UI/Form/Button/Button';
 import { PrimaryInput, SecondaryInput } from '../../UI/Form/Input/Input';
 import { Icon } from '@iconify/react';
+import React, {useRef, useState} from 'react';
 
 
 const Teacher = () => {
-
-    const sampleContainer = {
-        width: '20rem',
-        margin: '4rem',
+    
+    const hiddenFileInput = useRef(null);    
+    const [fileUrl, setFileUrl] = useState(["https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png"]);
+    
+    
+    let imageHandle=(event)=>{
+        const imageFile = event.target.files[0];
+        const imageUrl = URL.createObjectURL(imageFile);
+        setFileUrl(imageUrl)
     }
+   
 
-  return (
-    <div>
-        <h2>Teacher Registration</h2>
-        <div style={sampleContainer}>
-            <h3>Primary Input</h3>
-            <PrimaryInput
-                label="First name"
-                type="text"
-                info="Este texto es de información"
-                error="Este texto es de error"
-            />
+    const upLoadButton =()=>{
+        hiddenFileInput.current.click();
+    }
+    
+    const styleInput={
+        width: '30vw',
+        
+    }
+    return (
+    <div className='box'>
+        <div className='box_Title'>
+            <h2>Teacher Registration</h2>
         </div>
-        <div style={sampleContainer}>
-            <h3>Secondary Input</h3>
+        
+        <div className="box_name">
+            <div className='teachers_name'>
+                <PrimaryInput
+                    label="First name"
+                    type="text"
+                    info=""
+                    error=""
+                />
+
+                <PrimaryInput
+                    label="Last name"
+                    type="text"
+                    info=""
+                    error=""
+                />
+            </div>
+            <div className='box_image'>
+                <div className='teacher_image'>
+                    <img src={fileUrl} onChange={imageHandle}></img>
+                </div>
+                   
+                 <input type="file" id ="Selectedinput" ref={hiddenFileInput} accept="image/*" onChange={imageHandle}></input>
+                <SecondaryButton cursor="pointer"  onClick ={upLoadButton}> Add Image</SecondaryButton>
+            </div>
+            
+        </div>
+        
+        <div className='box_logindata'>
             <SecondaryInput
-                label="First name"
+                label="Email"
                 type="text"
-                info="Este texto es de información"
-                error="Este texto es de error"
+                info=""
+                error=""
+            />
+            <div className='pass'>
+                <SecondaryInput
+                    label="Password"
+                    type="password"
+                    info=""
+                    error=""
+                    style={styleInput}
+                    
+                />
+
+                <SecondaryInput
+                    label="Confirm Pass"
+                    type="password"
+                    info=""
+                    error=""
+                    style={styleInput}
+                />
+            </div>
+            
+
+        </div>
+
+        <div className='box_title'>
+                
+                <div className='title_expertise'>
+                    <div>
+                        <PrimaryInput
+                            label="Title"
+                            type="text"
+                            info=""
+                            error=""
+                            
+                        />
+                    </div>
+                   
+                    <div>
+                        <PrimaryInput
+                            label="Expertise"
+                            type="text"
+                            info=""
+                            error=""
+                        />
+                    </div>
+                   
+                </div>
+                
+
+            <SecondaryInput
+                label="Description"
+                type="text"
+                info=""
+                error=""
             />
         </div>
-        <div style={sampleContainer}>
-            <h3>Sample Buttons</h3>
-            <BaseButton style={{backgroundColor: "#abc", fontWeight: 'bold'}}>
-                Custom Black
-            </BaseButton>
-            <BaseButton style={{backgroundColor: "gray", fontWeight: 'bold'}}>
-                <Icon icon='cil:save' />
-                With Icon
-            </BaseButton>
-            <PrimaryButton>
-                Primary
-            </PrimaryButton>
-            <SecondaryButton>
-                Secondary
-            </SecondaryButton>
+
+        <div className='box_institution'>
+            <div className='institutions'>
+                <PrimaryInput
+                    label="Institutions"
+                    type="text"
+                    info=""
+                    error=""
+                    style={styleInput}
+                />
+
+                <div>
+                    <BaseButton style={{backgroundColor: "blue", fontWeight: 'bold'}}>
+                        <Icon icon="carbon:add-filled" />
+                        ADD
+                    </BaseButton>
+
+                    <BaseButton style={{backgroundColor: "gray", fontWeight: 'bold'}}>
+                        <Icon icon="fa6-solid:trash-can" />
+                            DEL
+                    </BaseButton>
+                </div>
+            </div>
+
+            <div className='plataforms'>
+                <PrimaryInput
+                    label="Plataforms"
+                    type="text"
+                    info=""
+                    error=""
+                    style={styleInput}
+                />
+                <div>
+                    <BaseButton style={{backgroundColor: "blue", fontWeight: 'bold'}}>
+                        <Icon icon="carbon:add-filled" />
+                            ADD
+                    </BaseButton>
+
+                    <BaseButton style={{backgroundColor: "gray", fontWeight: 'bold'}}>
+                        <Icon icon="fa6-solid:trash-can" />
+                            DEL
+                    </BaseButton>
+                </div>
+           
+            </div>
+
+        </div>
+
+        <div className='box_buttons'>
+            <div className='button'>
+                <PrimaryButton>
+                    ACCEPT
+                </PrimaryButton>
+            </div>
+            
+            <div>
+                <SecondaryButton>
+                    CANCEL
+                </SecondaryButton>
+            </div>
+            
         </div>
     </div>
   )
