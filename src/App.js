c1bc6ad00614f18c03beee5cbcfd096543c0278e
AM98-20 <alexmrtnz902@gmail.com>
2022-03-24 18:01:52 -0600

    Interface Option and student registration

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import SamplePage from './components/Views/Sample/SamplePage';
+import RegisterOption from './components/Views/Register/Options/Register.optionsPage';
 import Login from './components/Views/Login/Login';
+import StudentRegister from './components/Views/Register/Students/Student.registrationPage'
 
 function App() {
   return (
       <Routes>
         <Route path='/' element={<SamplePage />} />
         <Route path='/login' element={<Login />} />
+        <Route path='/register-options' element={<RegisterOption/>} />
+        <Route path='/student-registration' element={<StudentRegister/>} />
       </Routes>
   );
 }
---
+++ src/components/Views/Register/Students/Student.registration.jsx
@@ -0,0 +1,61 @@
+import { BaseButton, PrimaryButton, SecondaryButton } from '../../../UI/Form/Button/Button';
+import { PrimaryInput, SecondaryInput } from '../../../UI/Form/Input/Input';
+import { Icon } from '@iconify/react';
+import './Student.registration.css'
+
+const StudentRegister = () => {
+
+    return (
+        <div className="container-signup">
+            <div className="forms-container">
+                <div className="signup-form">
+                    <form action="#" className="sign-in-form">
+                        <h2 className="title">Sign up</h2>
+                        <div className="input-field">
+                            <i className="fas fa-user"></i>
+                            <PrimaryInput type="text" placeholder="Nombre(s)" />
+                        </div>
+                        <div className="input-field">
+                            <i className="fas fa-lock"></i>
+                            <PrimaryInput type="text" placeholder="Apellido(s)" />
+                        </div>
+                        <div className="input-field">
+                            <i className="fas fa-lock"></i>
+                            <PrimaryInput type="text" placeholder="E-mail" />
+                        </div>
+                        <div className="input-field">
+                            <i className="fas fa-lock"></i>
+                            <PrimaryInput type="password" placeholder="Password" />
+                        </div>
+                        <div className="input-field">
+                            <i className="fas fa-lock"></i>
+                            <PrimaryInput type="password" placeholder="Confirm password" />
+                        </div>
+                        <PrimaryButton className="btn solid" >
+                            Signup
+                        </PrimaryButton>
+
+                    </form>
+                </div>
+            </div>
+
+            <div className="panels-container">
+                <div className="panel left-panel">
+                    <div className="content">
+                        <h3>STUDENT</h3>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis,
+                            ex ratione. Aliquid!
+                        </p>
+                    </div>
+                    <img src="/student.svg" className="image" alt="" />
+                </div>
+            </div>
+        </div>
+
+
+
+    )
+}
+
+export default StudentRegister;
