075e666fe97fd3784f061e009c243aef2d5d72f2
Oscar Andrade <oandrade57@gmail.com>
2022-04-02 00:38:41 -0600

    Validation Formik and Yup

--- src/components/Views/Teacher/Teacher.js
+++ src/components/Views/Teacher/Teacher.js
@@ -3,9 +3,18 @@ import { BaseButton, PrimaryButton, SecondaryButton } from '../../UI/Form/Button
 import { PrimaryInput, SecondaryInput } from '../../UI/Form/Input/Input';
 import { Icon } from '@iconify/react';
 import React, {useRef, useState} from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import FileBase64 from 'react-file-base64'
 
 
-const Teacher = () => {
+
+
+const Teacher = ({
+    txtFirstValue,
+    txtLastnameValue,
+    onChange:onChangeHandler,
+    onConfirmClick}) => {
     
     const hiddenFileInput = useRef(null);    
     const [fileUrl, setFileUrl] = useState(["https://www.pngall.com/wp-content/uploads/5/User-Profile-PNG-High-Quality-Image.png"]);
@@ -14,13 +23,76 @@ const Teacher = () => {
     let imageHandle=(event)=>{
         const imageFile = event.target.files[0];
         const imageUrl = URL.createObjectURL(imageFile);
-        setFileUrl(imageUrl)
+        setFileUrl(imageUrl);
+        console.log(imageFile);
     }
-   
 
     const upLoadButton =()=>{
         hiddenFileInput.current.click();
     }
+
+    const filebase =(value)=>{
+        setFileUrl(value);
+        console.log(value);
+    }
+
+    const validation = Yup.object({
+        firstname: Yup.string()
+            .min(3,"Minimum 3 characters")
+            .max(15, 'Must be 15 characters or less')
+            .required("Name Requiered"),
+        lastname: Yup.string()
+            .min(3,"Minimum 3 characters")
+            .max(15, 'Must be 15 characters or less')
+            .required("Lastname Requiered"),
+        email: Yup.string()
+            .email("Invalid Email")
+            .required("Email Requiered"),
+        password:Yup.string()
+            .min(8,"Password must be 8 character minimum")
+            .matches(/^((?=\S*?[A-Z])(?=\S*?[a-z])(?=\S*?[0-9]).{8,})\S$/,
+                "Must Contain 8 characters, 1 uppercase, 1 lowercase, 1 number")
+            .required("Password Requiered"),
+        confirmpass: Yup.string()
+            .oneOf([Yup.ref('password'),null],"Passwords must match"),
+        title: Yup.string()
+            .max(20, "Title must be less than 20 characters"),
+        expertise: Yup.string()
+            .max(20, "Expertise field must be less than 20 characters"),
+        description: Yup.string()
+            .max(40, "40 characters max is permitted"),
+        institutions: Yup.string()
+            .max(20, "Institution must be less than 20 characters"),
+        plataform: Yup.string()
+            .max(20, "Plataform must be less than 20 characters"),
+        image: Yup.mixed()  
+        
+      });
+    
+      const initial ={
+        firstname: '',
+        lastname:'',
+        email:'',
+        password: '',
+        confirmpass: '',
+        title:'',
+        expertise:'',
+        description:'',
+        institutions:'',
+        plataform:'',
+        image: ''
+    }
+
+    const formik = useFormik({
+        initialValues: initial,
+        validationSchema: validation,
+        onSubmit: values => {
+          console.log(values);
+          
+        },
+      });
+          
+    
     
     const styleInput={
         width: '30vw',
@@ -31,159 +103,211 @@ const Teacher = () => {
         <div className='box_Title'>
             <h2>Teacher Registration</h2>
         </div>
-        
-        <div className="box_name">
-            <div className='teachers_name'>
-                <PrimaryInput
-                    label="First name"
-                    type="text"
-                    info=""
-                    error=""
-                />
-
-                <PrimaryInput
-                    label="Last name"
-                    type="text"
-                    info=""
-                    error=""
-                />
-            </div>
-            <div className='box_image'>
-                <div className='teacher_image'>
-                    <img src={fileUrl} onChange={imageHandle}></img>
-                </div>
-                   
-                 <input type="file" id ="Selectedinput" ref={hiddenFileInput} accept="image/*" onChange={imageHandle}></input>
-                <SecondaryButton cursor="pointer"  onClick ={upLoadButton}> Add Image</SecondaryButton>
-            </div>
-            
-        </div>
-        
-        <div className='box_logindata'>
-            <SecondaryInput
-                label="Email"
-                type="text"
-                info=""
-                error=""
-            />
-            <div className='pass'>
-                <SecondaryInput
-                    label="Password"
-                    type="password"
-                    info=""
-                    error=""
-                    style={styleInput}
+        <form onSubmit={formik.handleSubmit}>
+            <div className="box_name">
+                <div className='teachers_name'>
+                    <PrimaryInput
+                        label="First name"
+                        type="text"
+                        info=""
+                        error={formik.errors.firstname}
+                        name = "firstname"
+                        value = {formik.values.firstname}
+                        onChange = {formik.handleChange}
+                        
+                    />
                     
+
+                    <PrimaryInput
+                        label="Last name"
+                        type="text"
+                        info=""
+                        error={formik.errors.lastname}
+                        name = "lastname"
+                        value = {formik.values.lastname}
+                        onChange = {formik.handleChange}
+                    />
+                </div>
+                <div className='box_image'>
+                    <div className='teacher_image'>
+                        <img src={fileUrl} onChange={imageHandle} name="image" ></img>
+                    </div>
+                    
+                    <input type="file" id ="Selectedinput" ref={hiddenFileInput} accept="image/*" onChange={imageHandle}></input>
+                    <SecondaryButton cursor="pointer"  onClick ={upLoadButton} type="button"> Add Image</SecondaryButton>
+                    
+                    <FileBase64
+                        multiple={ false }
+                        onDone={({base64})=>filebase(base64) }
+                         />
+                </div>
+                
+            </div>
+            
+            <div className='box_logindata'>
+                <SecondaryInput
+                    label="Email"
+                    type="text"
+                    info=""
+                    error={formik.errors.email}
+                    name = "email"
+                    value = {formik.values.email}
+                    onChange = {formik.handleChange}
                 />
+                <div className='pass'>
+                    <SecondaryInput
+                        label="Password"
+                        type="password"
+                        info=""
+                        style={styleInput}
+                        error={formik.errors.password}
+                        name = "password"
+                        value = {formik.values.password}
+                        onChange = {formik.handleChange}
+                        
+                    />
+
+                    <SecondaryInput
+                        label="Confirm Pass"
+                        type="password"
+                        info=""
+                        error={formik.errors.confirmpass}
+                        name = "confirmpass"
+                        value = {formik.values.confirmpass}
+                        onChange = {formik.handleChange}
+                        style={styleInput}
+                    />
+                </div>
+                
+
+            </div>
+
+            <div className='box_title'>
+                    
+                    <div className='title_expertise'>
+                        <div>
+                            <PrimaryInput
+                                label="Title"
+                                type="text"
+                                info=""
+                                error={formik.errors.title}
+                                name = "title"
+                                value = {formik.values.title}
+                                onChange = {formik.handleChange}
+                                
+                            />
+                        </div>
+                    
+                        <div>
+                            <PrimaryInput
+                                label="Expertise"
+                                type="text"
+                                info=""
+                                error={formik.errors.expertise}
+                                name = "expertise"
+                                value = {formik.values.expertise}
+                                onChange = {formik.handleChange}
+                            />
+                        </div>
+                    
+                    </div>
+                    
 
                 <SecondaryInput
-                    label="Confirm Pass"
-                    type="password"
-                    info=""
-                    error=""
-                    style={styleInput}
-                />
-            </div>
-            
-
-        </div>
-
-        <div className='box_title'>
-                
-                <div className='title_expertise'>
-                    <div>
-                        <PrimaryInput
-                            label="Title"
-                            type="text"
-                            info=""
-                            error=""
-                            
-                        />
-                    </div>
-                   
-                    <div>
-                        <PrimaryInput
-                            label="Expertise"
-                            type="text"
-                            info=""
-                            error=""
-                        />
-                    </div>
-                   
-                </div>
-                
-
-            <SecondaryInput
-                label="Description"
-                type="text"
-                info=""
-                error=""
-            />
-        </div>
-
-        <div className='box_institution'>
-            <div className='institutions'>
-                <PrimaryInput
-                    label="Institutions"
+                    label="Description"
                     type="text"
                     info=""
-                    error=""
-                    style={styleInput}
+                    error={formik.errors.description}
+                    name = "description"
+                    value = {formik.values.description}
+                    onChange = {formik.handleChange}
                 />
-
-                <div>
-                    <BaseButton style={{backgroundColor: "blue", fontWeight: 'bold'}}>
-                        <Icon icon="carbon:add-filled" />
-                        ADD
-                    </BaseButton>
-
-                    <BaseButton style={{backgroundColor: "gray", fontWeight: 'bold'}}>
-                        <Icon icon="fa6-solid:trash-can" />
-                            DEL
-                    </BaseButton>
-                </div>
             </div>
 
-            <div className='plataforms'>
-                <PrimaryInput
-                    label="Plataforms"
-                    type="text"
-                    info=""
-                    error=""
-                    style={styleInput}
-                />
-                <div>
-                    <BaseButton style={{backgroundColor: "blue", fontWeight: 'bold'}}>
-                        <Icon icon="carbon:add-filled" />
-                            ADD
-                    </BaseButton>
+            <div className='box_institution'>
+                <div className='institutions'>
+              
+                    <div className='inst'>
+                        <PrimaryInput
+                            label="Institutions"
+                            type="text"
+                            info=""
+                            error={formik.errors.institutions}
+                            name = "institutions"
+                            value = {formik.values.institutions}
+                            onChange = {formik.handleChange}
+                            style={styleInput}
+                        />
 
-                    <BaseButton style={{backgroundColor: "gray", fontWeight: 'bold'}}>
-                        <Icon icon="fa6-solid:trash-can" />
-                            DEL
-                    </BaseButton>
-                </div>
-           
-            </div>
+                        <div>
+                            <BaseButton type="button" style={{backgroundColor: "blue", fontWeight: 'bold'}}>
+                                <Icon icon="carbon:add-filled" />
+                                ADD
+                            </BaseButton>
 
-        </div>
+                            <BaseButton type="button" style={{backgroundColor: "gray", fontWeight: 'bold'}}>
+                                <Icon icon="fa6-solid:trash-can" />
+                                    DEL
+                            </BaseButton>
+                    </div>
+                    </div>
 
-        <div className='box_buttons'>
-            <div className='button'>
-                <PrimaryButton>
-                    ACCEPT
-                </PrimaryButton>
-            </div>
+                    
+
+                    
             
-            <div>
-                <SecondaryButton>
-                    CANCEL
-                </SecondaryButton>
-            </div>
+                </div>
+
+                <div className='plataforms'>
+
+                    <div className='plats'>
+                    <PrimaryInput
+                        label="Plataforms"
+                        type="text"
+                        info=""
+                        error={formik.errors.plataform}
+                        name = "plataform"
+                        value = {formik.values.plataform}
+                        onChange = {formik.handleChange}
+                        style={styleInput}
+                    />
+                    <div>
+                        <BaseButton style={{backgroundColor: "blue", fontWeight: 'bold'}}>
+                            <Icon icon="carbon:add-filled" />
+                                ADD
+                        </BaseButton>
+
+                        <BaseButton style={{backgroundColor: "gray", fontWeight: 'bold'}}>
+                            <Icon icon="fa6-solid:trash-can" />
+                                DEL
+                        </BaseButton>
+                    </div>
+                    </div>
+                    
             
-        </div>
+                </div>
+
+            </div>
+
+            <div className='box_buttons'>
+                <div className='button'>
+                    <PrimaryButton type="submit" onClick={onConfirmClick}>
+                        ACCEPT
+                    </PrimaryButton>
+                </div>
+                
+                <div>
+                    <SecondaryButton>
+                        CANCEL
+                    </SecondaryButton>
+                </div>
+                
+            </div>
+        </form>
     </div>
   )
 }
 
 export default Teacher;
+
+
--- src/components/Views/Teacher/TeacherPage.js
+++ src/components/Views/Teacher/TeacherPage.js
@@ -1,13 +1,55 @@
 import Teacher from './Teacher'
 import Header from '../../UI/Header/Header'
 import Page from '../../UI/Page/Page'
+import {useState} from 'react';
+import {publicAxios} from '../../../Lib/apiClient';
+
+
 
 const TeacherPage = () => {
+  
+  const [firstname, setfirstname] = useState('');
+  const [txtSurname, setTxtSurname] = useState('');
+
+  const onConfirm = async (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    try{
+        
+        const data = publicAxios.post(
+            '/api/v1/auth/teacher-signup',
+            
+            {
+                name:firstname,
+                surname:txtSurname
+            });
+    
+        console.log('Signin Request: ',data)
+    }catch(ex){
+        console.log('Error on Signin submit',ex)
+    }
+
+  }
+
+  const onChangeHandler = ({target: {name,value}})=>{
+    switch (name){
+        case 'firstname':
+          setfirstname(value);
+        break;
+        
+        case 'txtSurname':
+          setTxtSurname(value);
+        break;
+        
+    }
+  }
   return (
     <Page
         header={<Header />}
     >
-        <Teacher />
+        <Teacher 
+          
+        />
     </Page>
   )
 }
