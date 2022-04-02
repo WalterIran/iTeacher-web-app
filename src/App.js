7dca8f64a94f2c0d6409467560f61dfb6133332f
kjmg05 <kenj_martinezg@unicah.edu>
2022-04-02 00:23:05 -0600

    Created public axios, api client and login success

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import SamplePage from './components/Views/Sample/SamplePage';
-import RegisterOption from './components/Views/Register/Options/Register.optionsPage';
-import Login from './components/Views/Login/Login';
-import TeacherPage from './components/Views/Teacher/TeacherPage';
-import Profile from './components/Views/Profile/ProfilePage';
-import Search from './components/Views/Search/SearchPage';
-import Review from './components/Views/Review/Review';
-import StudentRegister from './components/Views/Register/Students/Student.registrationPage'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import SamplePage from "./components/Views/Sample/SamplePage";
+import RegisterOption from "./components/Views/Register/Options/Register.optionsPage";
+import Login from "./components/Views/Login/Login";
+import TeacherPage from "./components/Views/Teacher/TeacherPage";
+import Profile from "./components/Views/Profile/ProfilePage";
+import Search from "./components/Views/Search/SearchPage";
+import Review from "./components/Views/Review/Review";
+import StudentRegister from "./components/Views/Register/Students/Student.registrationPage";
 
 function App() {
   return (
-      <Routes>
-        <Route path='/' element={<SamplePage />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/teacher-registration' element={<TeacherPage />} />
-        <Route path='/profile' element={<Profile />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/review' element={<Review />} />
-        <Route path='/register-options' element={<RegisterOption/>} />
-        <Route path='/student-registration' element={<StudentRegister/>} />
-      </Routes>
+    <Routes>
+      <Route path="/" element={<SamplePage />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/teacher-registration" element={<TeacherPage />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/review" element={<Review />} />
+      <Route path="/register-options" element={<RegisterOption />} />
+      <Route path="/student-registration" element={<StudentRegister />} />
+    </Routes>
   );
 }
 
--- src/components/Views/Login/Login.jsx
+++ src/components/Views/Login/Login.jsx
@@ -1,30 +1,82 @@
-import React from "react";
+import React, { useState, useContext } from "react";
 import "./Login.css";
 import { PrimaryButton, SecondaryButton } from "../../UI/Form/Button/Button";
 import { PrimaryInput } from "../../UI/Form/Input/Input";
+import AuthContext from "../../../context/AuthProvider";
+import { publicAxios } from "../../../Lib/apiClient";
+const LOGIN_URL = "/api/v1/auth/login";
 
 const Login = () => {
+  const { setAuth } = useContext(AuthContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await publicAxios.post(LOGIN_URL, {
+        email: email,
+        password: password,
+      });
+      const accessToken = response?.data?.accessToken;
+      const userType = response?.data?.userType;
+      setAuth({ email, password, userType, accessToken });
+      setEmail('')
+      setPassword('')
+      alert("Inicio de sesion exitoso")
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
-    <div className="outer-div">
-      <div className="img-div">
-        <img src={"https://raw.githubusercontent.com/WalterIran/iteacher-web-app/main/public/app-logo.png"} width="200" height="200" />
-      </div>
-      <div className="inner-div">
-        <div className="input-email">
-          <PrimaryInput type="text" placeholder="EMAIL" />
+    <>
+        <div className="outer-div">
+          <div className="img-div">
+            <img
+              src={
+                "https://raw.githubusercontent.com/WalterIran/iteacher-web-app/main/public/app-logo.png"
+              }
+              width="200"
+              height="200"
+            />
+          </div>
+          <div className="inner-div">
+            <div className="input-email">
+              <PrimaryInput
+                type="text"
+                id="email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                required
+                placeholder="EMAIL"
+              />
+            </div>
+            <div className="input-pass">
+              <PrimaryInput
+                type="password"
+                id="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+                placeholder="PASSWORD"
+              />
+            </div>
+            <div className="button-login">
+              <PrimaryButton style={{ width: "200px" }} onClick={handleSubmit}>
+                Log In
+              </PrimaryButton>
+            </div>
+            <div className="button-signin">
+              <PrimaryButton style={{ width: "200px" }}>Sign Up</PrimaryButton>
+            </div>
+          </div>
         </div>
-        <div className="input-pass">
-          <PrimaryInput type="password" placeholder="PASSWORD" />
-        </div>
-        <div>Forgot Password</div>
-        <div className="button-login">
-          <PrimaryButton style = {{width: '200px'}}>Log In</PrimaryButton>
-        </div>
-        <div className="button-signin">
-          <PrimaryButton style = {{width: '200px'}}>Sign In</PrimaryButton>
-        </div>
-      </div>
-    </div>
+      
+    </>
   );
 };
 
